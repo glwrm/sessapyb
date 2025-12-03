ee17a5aedd9ba5f7cd7010bce8bc1c5a7add204e
glwørm <glowwqrm@gmail.com>
2025-12-03 17:35:06 -0500

    Add eruda bookmarklet

---
+++ bookmarklets/eruda.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bvar%20s=document.createElement('script');s.src='https://cdn.jsdelivr.net/npm/eruda';s.onload=function()%7Beruda.init();%7D;document.body.appendChild(s);%7D)();
